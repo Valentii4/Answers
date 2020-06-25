a84f2c090b3bc6d434b14b39b865d003454cc27f
Valentin <valentinhhf@gmail.com>
2020-06-25 11:16:56 +0500

    Initial Commit

---
+++ YourHelper WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  YourHelper WatchKit Extension
+//
+//  Created by Valentin Mironov on 25.06.2020.
+//  Copyright © 2020 Valentin Mironov. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
